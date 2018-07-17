f35df796bd66ffa47c468da899c0b43f82b6c473
Martin3Young <>
2018-07-17 14:55:08 +0800

    更新点击事件

--- app/src/main/java/yunfucloud/com/myapp/activity/DiyActivity.java
+++ app/src/main/java/yunfucloud/com/myapp/activity/DiyActivity.java
@@ -3,8 +3,10 @@ package yunfucloud.com.myapp.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import yunfucloud.com.myapp.R;
+import yunfucloud.com.myapp.view.GranzortThreeView;
 
 /**
  * Created by jingjing on 2017/11/30.
@@ -13,10 +15,19 @@ import yunfucloud.com.myapp.R;
 public class DiyActivity extends AppCompatActivity {
 
 
+    GranzortThreeView my_view;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diy);
+        my_view = (GranzortThreeView) findViewById(R.id.my_view);
+        my_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                my_view.reSet();
+            }
+        });
     }
 
 }
